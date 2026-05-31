bcc2a2aaebcd92e130753f053aadcdd3199e3827
Daniele F. Dias de Souza <diasfdaniele@gmail.com>
2026-05-30 23:38:13 -0300

    Correção de bug no cadastro de usuários.

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,22 @@
 'use strict';
 
-import { auth, db } from './firebase.js';
-import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+
+// Configuração do Firebase (copie do seu firebase.js se necessário)
+const firebaseConfig = {
+  apiKey: "AIzaSyC-YG19WRKIUFE__7608NYqrrehSWr8Zd0",
+  authDomain: "empr-e.firebaseapp.com",
+  projectId: "empr-e",
+  storageBucket: "empr-e.appspot.com",
+  messagingSenderId: "196840931732",
+  appId: "1:196840931732:web:d316628d34006a8e37cf81"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 const googleLoginButton = document.getElementById('btn-google-login');
 // Login com Google
 if (googleLoginButton) {
